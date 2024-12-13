0dd864deb23a733638ab60cbd98f388f208b0197
Anastasia <nnefedova1@icloud.com>
2024-12-12 22:39:27 -0800

    Rename module_1_6.py to Module 1/module_1_6.py

---
+++ Module 1/module_1_6.py
@@ -0,0 +1,4 @@
+number_of_completed_homework_assignments = 12
+number_of_hours_spent = 1.5
+course_name = "Python"
+print("Курс: Python,", "всего задач:", (number_of_completed_homework_assignments ), ", затрачено часов:", (number_of_hours_spent), ", среднее время выполнения:", (number_of_hours_spent/number_of_completed_homework_assignments), 'часа.')
